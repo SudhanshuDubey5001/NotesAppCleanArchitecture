58234875194d9847c88a6de19e2b892014f74b2c
SudhanshuDubey5001 <dubey0itwork@gmail.com>
2023-10-09 15:11:17 +0100

    first commit

---
+++ app/src/main/java/com/sudhanshu/notesappcleanarch/NotesAppApplication.kt
@@ -0,0 +1,8 @@
+package com.sudhanshu.notesappcleanarch
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class NotesAppApplication : Application() {
+}
